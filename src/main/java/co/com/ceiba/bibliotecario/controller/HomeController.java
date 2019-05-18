ac45ae82f4618f0e08c4615a9dbdce94f130bb09
Eduardo <e2mb90@gmail.com>
2019-05-18 13:19:30 -0500

    Se agrega la estructura base del proyecto

---
+++ src/main/java/co/com/ceiba/bibliotecario/controller/HomeController.java
@@ -0,0 +1,14 @@
+package co.com.ceiba.bibliotecario.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String home() {
+		return "We can do it!";
+	}
+
+}
